af5006dd100aceebf4fd792015ee1d67ddbfa973
Kevin Kihara <38385590+developerkevo@users.noreply.github.com>
2020-11-03 12:08:54 +0300

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,1 +1,1 @@
-<?php $link = mysqli_connect('localhost','root','nexttech','loan') or die('Unable to Connect to Database'); ?>
+<?php $link = mysqli_connect('localhost','root','','') or die('Unable to Connect to Database'); ?>
